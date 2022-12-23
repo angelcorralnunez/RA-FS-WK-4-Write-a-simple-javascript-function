f5f90637c80ec61ae5b8fa9e35279cdcfa1ef643
Angelena Nunez <angelcorralnunez@gmail.com>
2022-12-23 11:59:51 -0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+/* Write a javascript code that prints out your name, height and country on the screen.
+
+1. Create an index.js file.
+
+2. Write a function that takes three(3) parameters (name, height and country)
+
+3. Write an alert in the function that prints out the inputs.
+
+4. Call the function and pass the three(3) parameter values into it.
+
+5. Push your codes to GitHub*/
+
+let stringArray = ["Angelena", "5ft 4in", "USA"];  
+  
+stringArray.forEach(function(str) {  
+    console.log(str);  
+    alert("Angelena, 5ft 4in, USA")
+}); 
